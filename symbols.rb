791a44cd8da7ce88c14378375a027d24d1229fed
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-23 17:03:50 -0500

    replacement & loadType

--- symbols.rb
+++ symbols.rb
@@ -5,14 +5,33 @@ replacementsDir = File.expand_path __FILE__
 if File.symlink? replacementsDir
     replacementsDir = File.readlink replacementsDir
 end
-$replacementsDir = File.dirname(replacementsDir) + '/replacements'
+$replacementsDir = File.dirname(replacementsDir) + '/replacements/'
+$replacementsDir = File.expand_path($replacementsDir,
+        File.dirname(__FILE__)) + '/'
 
 #########
 
 $symbols = Hash.new
 
+def loadType(type)
+    $symbols[type] = Hash.new
+    File.read($replacementsDir+type).split("\n").each do |l|
+        name, sym = l.split ':'
+        $symbols[type][name] = sym
+    end
+end
+
 def replacement(text)
-    text #DUMMY
+    type, symbol = text.split '/', 2
+    unless $symbols[type]
+        loadType type
+    end
+
+    if $symbols[type] and $symbols[type][symbol]
+        $symbols[type][symbol]
+    else
+        "{* Symbol not found: #{symbol}}"
+    end
 end
 
 #########
