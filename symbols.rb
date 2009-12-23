4f0c538744d420f0377fb80a94f0bd6f9c60c769
Jeffrey Aylesworth <jeffrey@aylesworth.ca>
2009-12-23 16:41:38 -0500

    framework

--- symbols.rb
+++ symbols.rb
@@ -5,5 +5,21 @@ replacementsDir = File.expand_path __FILE__
 if File.symlink? replacementsDir
     replacementsDir = File.readlink replacementsDir
 end
-replacementsDir = File.dirname(replacementsDir) + '/replacements'
+$replacementsDir = File.dirname(replacementsDir) + '/replacements'
 
+#########
+
+$symbols = Hash.new
+
+def replacement(text)
+    text #DUMMY
+end
+
+#########
+
+$stdin.each do |line|
+    line.gsub! /\{(.+?)\}/ do
+        replacement $1
+    end
+    puts line
+end
